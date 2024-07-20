2419047bc2eee00466b8384f7c6845a4d611d65b
Yizhuo <yizh.zhai@gmail.com>
2024-07-20 00:05:23 -0400

    add test for version match

--- src/eval.rs
+++ src/eval.rs
@@ -179,3 +179,163 @@ fn pre_is_compatible(cmp: &Comparator, ver: &Version) -> bool {
         && cmp.patch == Some(ver.patch)
         && !cmp.pre.is_empty()
 }
+
+#[cfg(test)]
+mod test_matches {
+    use super::*;
+    use crate::{Comparator, Op, Prerelease, Version, BuildMetadata};
+    #[test]
+    fn test_matches_caret_major() {
+        let major_version = 1;
+
+        let major_1  = 1;
+        let minor_1 = 0;
+        let patch_1  = 0;
+        //let rug_fuzz_2 = 0;
+        
+        let major_2 = 2;
+        let minor_2 = 0;
+        let patch_2 = 0;
+        let cmp = Comparator {
+            op: Op::Caret,
+            major: major_version,
+            minor: None,
+            patch: None,
+            pre: Prerelease::EMPTY,
+        };
+        let ver = Version::new(major_1, minor_1, patch_1);
+        assert!(matches_caret(& cmp, & ver));
+        let ver = Version::new(major_2, minor_2, patch_2);
+        assert!(! matches_caret(& cmp, & ver));
+
+    }
+    #[test]
+    fn test_matches_caret_minor() {
+        let major_version = 1;
+        let minor_version = 2;
+
+        let major_1 = 1;
+        let minor_1 = 2;
+        let patch_1 = 0;
+
+        let major_2 = 2;
+        let minor_2 = 0;
+        let patch_2 = 0;
+
+        let major_3 = 1;
+        let minor_3 = 3;
+        let patch_3 = 0;
+
+        let cmp = Comparator {
+            op: Op::Caret,
+            major: major_version,
+            minor: Some(minor_version),
+            patch: None,
+            pre: Prerelease::EMPTY,
+        };
+        let ver = Version::new(major_1, minor_1, patch_1);
+        assert!(matches_caret(& cmp, & ver));
+        let ver = Version::new(major_2, minor_2, patch_2);
+        assert!(! matches_caret(& cmp, & ver));
+        let ver = Version::new(major_3, minor_3, patch_3);
+        assert!(matches_caret(& cmp, & ver));
+    }
+    #[test]
+    fn test_matches_caret_patch() {
+        let major_0 = 0;
+        let minor_0 = 2;
+        let patch_0 = 3;
+
+        let major_1 = 0;
+        let minor_1 = 2;
+        let patch_1 = 3;
+
+        let major_2 = 0;
+        let minor_2 = 3;
+        let patch_2 = 0;
+
+        let major_3 = 0;
+        let minor_3 = 2;
+        let patch_3 = 4;
+        let cmp = Comparator {
+            op: Op::Caret,
+            major: major_0,
+            minor: Some(minor_0),
+            patch: Some(patch_0),
+            pre: Prerelease::EMPTY,
+        };
+        let ver = Version::new(major_1, minor_1, patch_1);
+        assert!(matches_caret(& cmp, & ver));
+        let ver = Version::new(major_2, minor_2, patch_2);
+        assert!(! matches_caret(& cmp, & ver));
+        let ver = Version::new(major_3, minor_3, patch_3);
+        assert!(matches_caret(& cmp, & ver));
+    }
+    #[test]
+    fn test_matches_caret_build() {
+        let major_version = 1;
+
+        let major_1 = 1;
+        let minor_1 = 0;
+        let patch_1 = 0;
+        let build_text_0 = "build";
+
+        let major_2 = 2;
+        let minor_2 = 0;
+        let patch_2 = 0;
+
+        let build_text_1 = "build";
+        let cmp = Comparator {
+            op: Op::Caret,
+            major: major_version,
+            minor: None,
+            patch: None,
+            pre: Prerelease::EMPTY,
+        };
+        let ver = Version {
+            major: major_1,
+            minor: minor_1,
+            patch: patch_1,
+            pre: Prerelease::EMPTY,
+            build: BuildMetadata::new(build_text_0).unwrap(),
+        };
+        assert!(matches_caret(& cmp, & ver));
+        let ver = Version {
+            major: major_2,
+            minor: minor_2,
+            patch: patch_2,
+            pre: Prerelease::EMPTY,
+            build: BuildMetadata::new(build_text_1).unwrap(),
+        };
+        assert!(! matches_caret(& cmp, & ver));
+    }
+    #[test]
+    fn test_matches_caret_zero_major() {
+        let major_version = 0;
+
+        let major_1 = 0;
+        let minor_1 = 1;
+        let patch_1 = 0;
+
+        let major_2 = 1;
+        let minor_2 = 0;
+        let patch_2 = 0;
+
+        let major_3 = 0;
+        let minor_3 = 0;
+        let patch_3 = 1;
+        let cmp = Comparator {
+            op: Op::Caret,
+            major: major_version,
+            minor: None,
+            patch: None,
+            pre: Prerelease::EMPTY,
+        };
+        let ver = Version::new(major_1, minor_1, patch_1);
+        assert!(matches_caret(& cmp, & ver));
+        let ver = Version::new(major_2, minor_2, patch_2);
+        assert!(! matches_caret(& cmp, & ver));
+        let ver = Version::new(major_3, minor_3, patch_3);
+        assert!(matches_caret(& cmp, & ver));
+    }
+}
